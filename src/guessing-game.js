6f1c51109b845d1b66bb03e75bb2a2b46e71936c
pirnataly <piroznatal@gmail.com>
2023-02-25 22:31:03 +0300

    feat:task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.min + this.max) / 2);
+        this.gues = this.mid;
+        return this.gues;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
-
 module.exports = GuessingGame;
